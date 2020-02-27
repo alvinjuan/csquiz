a1b48712cfc565826f6eccc996d953b0ee39054a
alvinjuan <alvinjasperj@gmail.com>
2020-02-26 18:06:04 -0800

    second commit

--- Main.java
+++ Main.java
@@ -29,7 +29,7 @@ public class Main {
         // sort
         for (int i=0; i<data.length; i++) {
             for (int j=0; j<data.length-i-1; j++) {
-                if (data[j]>data[j+1]) {
+                if (data[j]<data[j+1]) {
                     swap(data, j);
                 }
             }
